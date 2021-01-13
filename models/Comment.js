841f835e12089fd79e818670b9c920e97bbec325
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2021-01-13 13:10:37 -0600

    nested replies into comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,30 @@
 // importing model framework
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 //importing date util
 const dateFormat = require('../utils/dateFormat');
+
+// reply schema
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  }
+);
 
 //creating comment schema
 const CommentSchema = new Schema({
@@ -14,8 +36,10 @@ const CommentSchema = new Schema({
     },
     createdAt: {
       type: Date,
-      default: Date.now
-    }
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
   });
   
   const Comment = model('Comment', CommentSchema);
